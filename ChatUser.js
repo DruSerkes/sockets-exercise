f59c13589dae8fedd7612e55b9f021e82f0e739c
Dru Serkes <DruSerkes@gmail.com>
2020-07-20 15:05:30 -0400

    user can type /joke to get a random dadjoke (private)

--- ChatUser.js
+++ ChatUser.js
@@ -2,74 +2,90 @@
 
 // Room is an abstraction of a chat channel
 const Room = require('./Room');
+const axios = require('axios');
 
 /** ChatUser is a individual connection from client -> server to chat. */
 
 class ChatUser {
-  /** make chat: store connection-device, rooom */
+	/** make chat: store connection-device, room */
 
-  constructor(send, roomName) {
-    this._send = send; // "send" function for this user
-    this.room = Room.get(roomName); // room user will be in
-    this.name = null; // becomes the username of the visitor
+	constructor(send, roomName) {
+		this._send = send; // "send" function for this user
+		this.room = Room.get(roomName); // room user will be in
+		this.name = null; // becomes the username of the visitor
 
-    console.log(`created chat in ${this.room.name}`);
-  }
+		console.log(`created chat in ${this.room.name}`);
+	}
 
-  /** send msgs to this client using underlying connection-send-function */
+	/** send msgs to this client using underlying connection-send-function */
 
-  send(data) {
-    try {
-      this._send(data);
-    } catch {
-      // If trying to send to a user fails, ignore it
-    }
-  }
+	send(data) {
+		try {
+			this._send(data);
+		} catch (e) {
+			// If trying to send to a user fails, ignore it
+			console.log(e);
+		}
+	}
 
-  /** handle joining: add to room members, announce join */
+	/** handle joining: add to room members, announce join */
 
-  handleJoin(name) {
-    this.name = name;
-    this.room.join(this);
-    this.room.broadcast({
-      type: 'note',
-      text: `${this.name} joined "${this.room.name}".`
-    });
-  }
+	handleJoin(name) {
+		this.name = name;
+		this.room.join(this);
+		this.room.broadcast({
+			type : 'note',
+			text : `${this.name} joined "${this.room.name}".`
+		});
+	}
 
-  /** handle a chat: broadcast to room. */
+	/** handle a chat: broadcast to room. */
 
-  handleChat(text) {
-    this.room.broadcast({
-      name: this.name,
-      type: 'chat',
-      text: text
-    });
-  }
+	handleChat(text) {
+		this.room.broadcast({
+			name : this.name,
+			type : 'chat',
+			text : text
+		});
+	}
 
-  /** Handle messages from client:
+	/** Handle messages from client:
    *
    * - {type: "join", name: username} : join
    * - {type: "chat", text: msg }     : chat
    */
 
-  handleMessage(jsonData) {
-    let msg = JSON.parse(jsonData);
+	async handleMessage(jsonData) {
+		let msg = JSON.parse(jsonData);
 
-    if (msg.type === 'join') this.handleJoin(msg.name);
-    else if (msg.type === 'chat') this.handleChat(msg.text);
-    else throw new Error(`bad message: ${msg.type}`);
-  }
+		if (msg.type === 'join') this.handleJoin(msg.name);
+		else if (msg.type === 'chat') this.handleChat(msg.text);
+		else if (msg.type === 'get-joke') await this.handleJoke();
+		else throw new Error(`bad message: ${msg.type}`);
+	}
 
-  /** Connection was closed: leave room, announce exit to others */
+	async handleJoke() {
+		const response = await axios.get(`https://icanhazdadjoke.com/`, { headers: { Accept: 'application/json' } });
+		const joke = response.data.joke;
+		console.log(joke);
+		this.send(
+			JSON.stringify({
+				type : 'joke',
+				name : 'Dad Joke',
+				text : joke
+			})
+		);
+	}
 
-  handleClose() {
-    this.room.leave(this);
-    this.room.broadcast({
-      type: 'note',
-      text: `${this.name} left ${this.room.name}.`
-    });
-  }
+	/** Connection was closed: leave room, announce exit to others */
+
+	handleClose() {
+		this.room.leave(this);
+		this.room.broadcast({
+			type : 'note',
+			text : `${this.name} left ${this.room.name}.`
+		});
+	}
 }
 
 module.exports = ChatUser;
